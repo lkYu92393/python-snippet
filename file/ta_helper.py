91bd305fb58373d95ec73421cb5d7acdd2455481
Jim LK Yu <gundam1945@hotmail.com>
2026-03-24 16:29:00 +0800

    add import

--- file/ta_helper.py
+++ file/ta_helper.py
@@ -2,6 +2,7 @@
 import ta
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 
 def ta_bbm(df, window=20, window_dev=2):
     # Initialize Bollinger Bands Indicator
